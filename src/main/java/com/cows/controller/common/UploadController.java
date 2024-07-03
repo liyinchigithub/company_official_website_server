d50f64ecc41a90dc86a2072ea84dd96d9622924b
liyinchi_gitlab <liyinchi@qq.com>
2024-07-04 00:18:38 +0800

    编辑基础信息，微信图片（文件上传）

--- src/main/java/com/cows/controller/common/UploadController.java
+++ src/main/java/com/cows/controller/common/UploadController.java
@@ -4,10 +4,7 @@ import com.cows.commons.api.BaseResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
