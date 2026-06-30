accef4ad8962e29158b670257c4d830c1b1644d2
李银池 <liyinchi@qq.com>
2026-06-30 11:23:54 +0800

    更新初始化init.sql

---
+++ init.sql
@@ -0,0 +1,156 @@
+USE springBootTest;
+
+CREATE TABLE `About` (
+                         `id` int(11) NOT NULL AUTO_INCREMENT,
+                         `imageUrl` varchar(255) NOT NULL,
+                         `description` varchar(255) DEFAULT NULL,
+                         `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                         `updateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                         `isDeleted` tinyint(1) DEFAULT '0',
+                         PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `Admins` (
+                          `id` int(11) NOT NULL AUTO_INCREMENT,
+                          `userName` varchar(255) NOT NULL,
+                          `password` varchar(255) NOT NULL,
+                          `permissions` varchar(255) DEFAULT NULL,
+                          `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                          `updateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                          `remarks` text,
+                          `isDeleted` tinyint(1) DEFAULT '0',
+                          `isEnable` tinyint(1) DEFAULT '1',
+                          PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `BasicInformation` (
+                                    `id` bigint NOT NULL AUTO_INCREMENT,
+                                    `phone` varchar(255) NOT NULL,
+                                    `email` varchar(255) NOT NULL,
+                                    `address` varchar(255) NOT NULL,
+                                    `weChatImage` varchar(255) DEFAULT NULL,
+                                    `icp` varchar(255) DEFAULT NULL,
+                                    `beianImage` varchar(255) DEFAULT NULL,
+                                    `publicSecurity` varchar(255) DEFAULT NULL,
+                                    `copyright` varchar(255) DEFAULT NULL,
+                                    `isDeleted` tinyint(1) DEFAULT '0',
+                                    `isEnable` tinyint(1) DEFAULT '1',
+                                    `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                    `updateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                    `homeTitle` varchar(255) DEFAULT NULL,
+                                    `homeDescription` text,
+                                    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `BrandAuthorizationCertificates` (
+                                                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                                                  `name` varchar(255) NOT NULL COMMENT '证书名称',
+                                                  `description` text COMMENT '证书描述',
+                                                  `imageUrl` varchar(255) DEFAULT NULL COMMENT '证书图片URL地址',
+                                                  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                                  `updateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                                                  `isDeleted` tinyint(1) DEFAULT '0' COMMENT '是否删除',
+                                                  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `Brands` (
+                          `id` int(11) NOT NULL AUTO_INCREMENT,
+                          `name` varchar(255) NOT NULL,
+                          `description` text,
+                          `logo` varchar(255) DEFAULT NULL,
+                          `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                          `updateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                          `isDeleted` tinyint(1) DEFAULT '0',
+                          PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `Businesses` (
+                              `id` int(11) NOT NULL AUTO_INCREMENT,
+                              `name` varchar(255) NOT NULL,
+                              `description` text NOT NULL,
+                              `contactInfo` varchar(255) NOT NULL,
+                              `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                              `updateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                              `isDeleted` tinyint(1) DEFAULT '0',
+                              PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `Carousels` (
+                             `id` int(11) NOT NULL AUTO_INCREMENT,
+                             `title` varchar(255) DEFAULT NULL,
+                             `sort_order` int(11) DEFAULT '0',
+                             `imageUrl` varchar(255) NOT NULL,
+                             `redirectUrl` varchar(255) DEFAULT NULL,
+                             `isEnabled` tinyint(1) DEFAULT '1',
+                             `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                             `updateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                             `isDeleted` tinyint(1) DEFAULT '0',
+                             PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `User` (
+                        `id` int(100) NOT NULL AUTO_INCREMENT,
+                        `userName` varchar(100) DEFAULT NULL,
+                        `password` varchar(100) DEFAULT NULL,
+                        `created` datetime DEFAULT CURRENT_TIMESTAMP,
+                        `isEnable` tinyint(1) DEFAULT '1',
+                        `wechat_openid` varchar(255) DEFAULT NULL,
+                        `isDeleted` tinyint(1) DEFAULT '0',
+                        PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=485 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `Orders` (
+                          `id` int(11) NOT NULL AUTO_INCREMENT,
+                          `userId` int(100) DEFAULT NULL,
+                          `totalPrice` decimal(10,2) NOT NULL,
+                          `status` varchar(50) DEFAULT NULL,
+                          `createTime` datetime DEFAULT CURRENT_TIMESTAMP,
+                          `updateTime` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                          PRIMARY KEY (`id`),
+                          KEY `userId` (`userId`),
+                          CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `User` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ProductCategories` (
+                                     `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '商品分类ID',
+                                     `name` varchar(255) NOT NULL COMMENT '商品分类名称',
+                                     `description` text COMMENT '商品分类描述',
+                                     `isEnabled` tinyint(1) DEFAULT '1' COMMENT '启用禁用状态',
+                                     `isDeleted` tinyint(1) DEFAULT '0' COMMENT '是否删除',
+                                     `updateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                     `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                     PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='商品分类表';
+
+CREATE TABLE `Products` (
+                            `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+                            `name` varchar(255) NOT NULL COMMENT '商品名称',
+                            `coverImage` varchar(255) DEFAULT NULL COMMENT '商品封面图片',
+                            `detailImages` json DEFAULT NULL COMMENT '商品详情图片',
+                            `description` text COMMENT '商品描述',
+                            `salePrice` decimal(10,2) NOT NULL COMMENT '销售价格',
+                            `costPrice` decimal(10,2) NOT NULL COMMENT '成本价格',
+                            `stockQuantity` int(11) NOT NULL COMMENT '库存数量',
+                            `brand` varchar(255) DEFAULT NULL COMMENT '商品品牌',
+                            `categoryId` int(11) DEFAULT NULL COMMENT '商品分类ID',
+                            `isAvailable` tinyint(1) DEFAULT '1' COMMENT '上下架状态',
+                            `isDeleted` tinyint(1) DEFAULT '0' COMMENT '是否删除',
+                            `updateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                            `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                            PRIMARY KEY (`id`),
+                            KEY `categoryId` (`categoryId`),
+                            CONSTRAINT `Products_ibfk_1` FOREIGN KEY (`categoryId`) REFERENCES `ProductCategories` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COMMENT='商品信息表';
+
+CREATE TABLE `user_location` (
+                                 `id` int(11) NOT NULL AUTO_INCREMENT,
+                                 `user_id` int(100) NOT NULL,
+                                 `latitude` decimal(9,6) DEFAULT NULL,
+                                 `longitude` decimal(9,6) DEFAULT NULL,
+                                 `address` varchar(255) DEFAULT NULL,
+                                 `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                 `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                 PRIMARY KEY (`id`),
+                                 KEY `user_id` (`user_id`),
+                                 CONSTRAINT `user_location_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
